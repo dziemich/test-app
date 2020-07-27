8a2f2b2846d20e4e075d6efbeb0a49373c9e0c79
Dziedzic <michal.dziedzic22@gmail.com>
2020-07-27 19:05:33 +0200

    feat: validation

--- src/main/scala/dziemich/calculator/GlobalTypes.scala
+++ src/main/scala/dziemich/calculator/GlobalTypes.scala
@@ -1,5 +1,18 @@
 package dziemich.calculator
 
+object ValidationError extends Enumeration {
+  type ErrorMessage = Value
+  val INPUT_TOO_SHORT: ErrorMessage = Value("The input expression is too short.")
+  val ILLEGAL_BEGINNING: ErrorMessage = Value("The input expression starts with an illegal character.")
+  val ILLEGAL_ENDING: ErrorMessage = Value("The input expression ends with an illegal character.")
+  val ILLEGAL_CHARACTER: ErrorMessage = Value("The input expression contains an illegal character.")
+  val UNBALANCED_PARENTHESES: ErrorMessage = Value("The parentheses in the input expression are not balanced")
+  val MISSING_OPERATOR: ErrorMessage = Value("There is a missing operator in the input expression")
+  val INVALID_OPERATOR_USAGE: ErrorMessage = Value("The input expression contains invalid usage of operators")
+}
+
 object GlobalTypes {
   type CalculationResult = Either[Exception, Long]
+  type ValidationResult = Either[ValidationError.Value, String]
 }
+
--- src/main/scala/dziemich/calculator/Validator.scala
+++ src/main/scala/dziemich/calculator/Validator.scala
@@ -1,40 +1,59 @@
-//package dziemich.calculator
-//
-//import scala.collection.mutable
-//
-//class Validator {
-//  val operators: Array[Char] = Array('+', '-', '*', '/')
-//  val charsAllowed: Array[Char] = Array('(', ')') ++ operators
-//
-//  def isAllowed(c: Char): Boolean = charsAllowed.contains(c) || c.isDigit
-//
-//  def validateFirst(c: Char): Boolean = c == '(' || c.isDigit
-//
-//  def validate(string: String): (Boolean, String) = {
-//    var cleaned = string.filterNot((x: Char) => x.isWhitespace)
-//    if (cleaned.length <= 2) return (false, cleaned)
-//    var stack: mutable.Stack[Char] = mutable.Stack()
-//
-//    val first = cleaned.charAt(0)
-//    if (first == '-') cleaned = "0".concat(cleaned)
-//    if (!validateFirst(first)) return (false, cleaned)
-//
-//    for (i <- 1 until cleaned.length) {
-//      val c = cleaned.charAt(i)
-//      if (!isAllowed(c)) return (false, cleaned)
-//      if (charsAllowed.contains(c) && cleaned.charAt(i - 1))
-//      c match {
-//        case '(' => stack.push(c)
-//        case ')' => {
-//          if (stack.top == '(') {
-//            stack.pop()
-//          } else return (false, cleaned)
-//        }
-//        case op if operators.contains(op) => {
-//          if (operators.contains(cleaned.charAt(i - 1))) return (false, cleaned)
-//        }
-//      }
+package dziemich.calculator
+
+import dziemich.calculator.GlobalTypes.ValidationResult
+
+import scala.collection.mutable
+
+class Validator {
+  val operators: Array[Char] = Array('+', '-', '*', '/')
+  val charsAllowed: Array[Char] = Array('(', ')') ++ operators
+
+  def isAllowed(c: Char): Boolean = charsAllowed.contains(c) || c.isDigit
+
+  def validateFirst(c: Char): Boolean = c == '(' || c.isDigit
+
+  def validateLast(c: Char): Boolean = c == ')' || c.isDigit
+
+  def validate(string: String): ValidationResult = {
+    var cleaned = string.filterNot((x: Char) => x.isWhitespace)
+    if (cleaned.length <= 2) return Left(ValidationError.INPUT_TOO_SHORT)
+    val stack: mutable.Stack[Char] = mutable.Stack()
+
+    val first = cleaned.charAt(0)
+    if (first == '(') stack.push(first)
+    val last = cleaned.last
+
+    if (!validateFirst(first)) return Left(ValidationError.ILLEGAL_BEGINNING)
+    if (!validateLast(last)) return Left(ValidationError.ILLEGAL_ENDING)
+
+    for (i <- 1 until cleaned.length) {
+      val current = cleaned.charAt(i)
+      val previous = cleaned.charAt(i - 1)
+
+      if (!isAllowed(current)) return Left(ValidationError.ILLEGAL_CHARACTER)
+      current match {
+        case '(' => stack.push(current)
+        case ')' => {
+          if (stack.isEmpty) return Left(ValidationError.UNBALANCED_PARENTHESES)
+          if (operators.contains(previous)) return Left(ValidationError.INVALID_OPERATOR_USAGE)
+          if (stack.top == '(') {
+            stack.pop()
+          } else return Left(ValidationError.UNBALANCED_PARENTHESES)
+        }
+        case '+' | '*' | '/' => {
+          if (
+            (List('(') ++ operators).contains(previous)) return Left(ValidationError.INVALID_OPERATOR_USAGE)
+        }
+        case '-' => if (operators.contains(previous)) return Left(ValidationError.INVALID_OPERATOR_USAGE)
+        case digit if digit.isDigit => {
+          if (previous == ')') return Left(ValidationError.MISSING_OPERATOR)
+        }
+      }
+    }
+//    if (last == ')') {
+//      if (stack.isEmpty) return Left(ValidationError.UNBALANCED_PARENTHESES)
+//      stack.pop()
 //    }
-//    return (stack.isEmpty, cleaned)
-//  }
-//}
+    if (stack.isEmpty) Right(cleaned) else Left(ValidationError.UNBALANCED_PARENTHESES)
+  }
+}
